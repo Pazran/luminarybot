2bf692803961712639d6dcd70858b1b86d82b41f
Pazran <lawrenerno@gmail.com>
2017-06-21 19:37:58 +0800

    better naming for variables and remove some unused import

--- LuminaryBot.py
+++ LuminaryBot.py
@@ -1,31 +1,31 @@
 #LuminaryBot.py
+
 import json
-import sys
 import time
 import codecs
-import time
-from datetime import datetime
-from unidecode import unidecode
+from urllib.request import urlopen
 from twython import Twython, TwythonError
 from twython import TwythonStreamer
-from urllib.request import urlopen
 from secret import *
 
 def quote():
-    e = urlopen("http://quotes.rest/qod.json?category=life")
-    reader1 = codecs.getreader("utf-8")
-    value = json.load(reader1(e))
-    quote_e = value['contents']['quotes'][0]['quote']
-    author_e = value['contents']['quotes'][0]['author']
+    # Retrieve quote of the day from the life category
+    urlA = urlopen("http://quotes.rest/qod.json?category=life")
+    readerA = codecs.getreader("utf-8")
+    valueA = json.load(readerA(urlA))
+    quoteA = valueA['contents']['quotes'][0]['quote']
+    authorA = valueA['contents']['quotes'][0]['author']
 
-    f = urlopen("http://quotes.rest/qod.json?category=inspire")
-    reader = codecs.getreader("utf-8")
-    values = json.load(reader(f))
-    quote_f = values['contents']['quotes'][0]['quote']
-    author_f = values['contents']['quotes'][0]['author']
+    # Retrieve quote of the dat from the inspire category
+    urlB = urlopen("http://quotes.rest/qod.json?category=inspire")
+    readerB = codecs.getreader("utf-8")
+    valueB = json.load(readerB(urlB))
+    quoteB = valueB['contents']['quotes'][0]['quote']
+    authorB = valueB['contents']['quotes'][0]['author']
 
-    return author_e, quote_e, author_f, quote_f
+    return authorA, quoteA, authorB, quoteB
 
+# Check bot credintal
 def auth():
     twitter = Twython(C_KEY,
                   C_SECRET, 
@@ -33,30 +33,33 @@ def auth():
                   A_TOKEN_SECRET)
     return twitter
 
+# Main function start here
 def main():
 
-    author_e, quote_e, author_f, quote_f = quote()
+    authorA, quoteA, authorB, quoteB = quote()
     twitter = auth()
-    tweet_e = ""
-    tweet_f = ""
+    tweetA = ""
+    tweetB = ""
 
-    if len(author_f) >= 1 and len(quote_f)+len(author_f) <= (140-6):
+    # Check if the first quote too long since twitter has 140 character limit
+    # 140-6 to reserve for SPACES
+    if len(authorA) >= 1 and len(quoteA)+len(authorA) <= (140-6):
         try:
-                    tweet_f = "\"" + quote_f + "\"\n\n- " + author_f
-                    twitter.update_status(status=tweet_f)
+                    tweetB = "\"" + quoteA + "\"\n\n- " + authorA
+                    twitter.update_status(status=tweetA)
                     print("\nTwitted one quote. Check the log for more info...")
-        except TwythonError as f:
-                    print(f)
+        except TwythonError as errA:
+                    print(errA)
     else:
         print("Skipped! The quote is too long.")
 
-    if len(author_e) >= 1 and len(quote_e)+len(author_e) <= (140-6):
+    if len(authorB) >= 1 and len(quoteB)+len(authorB) <= (140-6):
         try:
-                    tweet_e = "\"" + quote_e + "\"\n\n- " + author_e
-                    twitter.update_status(status=tweet_e)
+                    tweetB = "\"" + quoteB + "\"\n\n- " + authorB
+                    twitter.update_status(status=tweetB)
                     print("\nTwitted one quote. Check the log for more info...")
-        except TwythonError as e:
-                    print(e)
+        except TwythonError as errB:
+                    print(errB)
     else:
         print("Skipped! The quote is too long.")
 		
--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 Twitter bot that will post two quotes everyday using Twython.
 This bot use REST API from https://theysaidso.com/api/ to retrieve the quote and the author.
 
+Fill in the auth keys in secret.py
+
 Required
 - twython 3.4.0
 - Unidecode 0.4.19
